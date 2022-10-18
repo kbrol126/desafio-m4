1374eed62307701b7fa6a2f4981af517b8e470ec
kbrol126 <nahu_sank06@hotmail.com>
2022-10-18 10:30:14 -0300

    arreglando imagenes rotas en el footer

--- reutilizables/footer/footer.js
+++ reutilizables/footer/footer.js
@@ -6,13 +6,13 @@ function importFooterComponent() {
     <div class="footer__redes">
    
         <a class="footer__links" href="https://www.instagram.com/nahuel126"> Instagram
-        <img src="/reutilizables/footer/Instagram.png" class="footer__img"></a>
+        <img src="./reutilizables/footer/Instagram.png" class="footer__img"></a>
    
         <a class="footer__links" href="https://www.linkedin.com/in/nahuel-cabral-0b20354a"> Linkedin
-        <img src="/reutilizables/footer/Linkedin.png" class="footer__img"></a>
+        <img src="./reutilizables/footer/Linkedin.png" class="footer__img"></a>
        
         <a class="footer__links" href="https://github.com/kbrol126"> GitHub
-        <img src="/reutilizables/footer/GitHub.png" class= "footer__img"></a>
+        <img src="./reutilizables/footer/GitHub.png" class= "footer__img"></a>
   </div>
   </section>`;
   return component;

745ac2b9c08dc0e8b802a813e94a398c10519648
kbrol126 <nahu_sank06@hotmail.com>
2022-10-19 11:33:30 -0300

    saque los html afuera de las carpetas para redirigir mas facil

--- reutilizables/header/header.js
+++ reutilizables/header/header.js
@@ -4,13 +4,13 @@ function importHeaderComponent() {
   <a href="/index.html" class="link__logo"><h4 class="header__logo" >NAHUE</h4></a>  
 
     <nav class="header__nav--desktop">
-    <a href="portfolio/index.html"><button class="header__button-hamburger--desktop" >
+    <a href="./portfolio.html"><button class="header__button-hamburger--desktop" >
     Portfolio
     </button></a>            
-    <a href="servicios/index.html"><button class="header__button-hamburger--desktop" >
+    <a href="./servicios.html"><button class="header__button-hamburger--desktop" >
   Servicios
   </button> </a>          
-  <a href="contacto/index.html"><button class="header__button-hamburger--desktop" >
+  <a href="./contacto.html"><button class="header__button-hamburger--desktop" >
   Contacto
   </button></a>
   </nav>
@@ -24,18 +24,18 @@ function importHeaderComponent() {
   </nav>
   <div class="header__nav__ventana">
   <div class="header__nav__ventana__close">
-  <a href="/index.html"><button class="header__nav__ventana__close--button " >
+  <a href="./index.html"><button class="header__nav__ventana__close--button " >
   
   </button>        </a>
   </div>
   <div class="header__nav_ventana_dropdown-menu">
-  <a href="portfolio/index.html"><button class="header__dropdown-menu__button" >
+  <a href="./portfolio.html"><button class="header__dropdown-menu__button" >
   Portfolio
   </button>            </a>
-  <a href="servicios/index.html"><button class="header__dropdown-menu__button" >
+  <a href="./servicios.html"><button class="header__dropdown-menu__button" >
   Servicios
   </button>           </a>
-  <a href="contacto/index.html"><button class="header__dropdown-menu__button" >
+  <a href="./contacto.html"><button class="header__dropdown-menu__button" >
   Contacto
   </button></a>
   </div>

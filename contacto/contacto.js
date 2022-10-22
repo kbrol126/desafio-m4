3e40484833cbf7fd3541fe4d9acc011eb7e65973
kbrol126 <nahu_sank06@hotmail.com>
2022-10-22 14:07:37 -0300

    arreglando el evento submit del form

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -19,5 +19,27 @@ function main() {
   botonEl[2].addEventListener("click", () => {
     ventanaEl.style.display = "flex";
   });
+  //selecciono el form
+  const contactoEl = document.querySelector(".contacto__form");
+  //escucho el evento submit y paro la ejecucion por default
+  contactoEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //recibo los datos y los paso a objeto para poder enviarlos
+    const formData = new FormData(contactoEl);
+    const formEntries = Object.fromEntries(formData.entries());
+    console.log(formEntries);
+    //consumo la api, y le envio los datos solicitados
+    fetch("https://apx-api.vercel.app/api/utils/dwf", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        to: "nahuel.cabral@hotmail.com",
+        message: `Nombre: ${formEntries.name}, Email: ${formEntries.email}, Mensaje: ${formEntries.message}`,
+      }),
+    }).then((data) => data.json());
+    //resutaura los valores por defecto del form
+    formEl.reset();
+  });
 }
+
 main();
